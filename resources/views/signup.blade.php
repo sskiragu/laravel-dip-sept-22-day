b89987b4d9d6b0c0c1081c738240a6c6b539085e
sskiragu <skiragu@gmail.com>
2023-06-13 16:12:21 +0300

    Initial commit

---
+++ resources/views/signup.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Sign up here</h1>
+    <div>
+        <form action="/process_signup">
+            <div>
+                <input type="text" name="username" placeholder="Enter username">
+            </div>
+            <div>
+                <input type="email" name="email" placeholder="Enter email">
+            </div>
+            <div>
+                <input type="password" name="password" placeholder="Enter password">
+            </div>
+            <div>
+                <input type="submit" name="signup" value="Signup">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/signup', function () {
+    return view('signup');
+});
+
+Route::get('/process_signup', [Controller::class, 'create']);
